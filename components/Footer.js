55e44d6238abfbb1908206ed78d6611f8f3d3e90
cobzeu <cobzeu@gmail.com>
2025-12-27 10:52:59 +0500

    Remove Twitter and Pinterest icons, keep only Instagram and Facebook

--- components/Footer.js
+++ components/Footer.js
@@ -34,18 +34,6 @@ export default function Footer() {
                                     <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
                                 </svg>
                             </a>
-                            <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="social-link" aria-label="Twitter">
-                                <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                                    <path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z"></path>
-                                </svg>
-                            </a>
-                            <a href="https://pinterest.com" target="_blank" rel="noopener noreferrer" className="social-link" aria-label="Pinterest">
-                                <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                                    <circle cx="12" cy="12" r="10"></circle>
-                                    <path d="M8 11.5c0 2.5 1.5 4.5 4 4.5 3 0 4-2 4-4 0-1.5-1-3-3-3s-3 1.5-3 3c0 .5 0 1 .5 1.5"></path>
-                                    <path d="m9 18 1-6"></path>
-                                </svg>
-                            </a>
                         </div>
                     </div>
 
