6619511eecc570710fd5bf034fec3007f6932d30
cobzeu <cobzeu@gmail.com>
2025-12-31 10:12:40 +0500

    Update Facebook link and fix men's collection slug in footer
    - Updated Facebook link to: https://www.facebook.com/share/1AeCBUgDf5/
    - Fixed men's collection link from men-stitched to men-unstitched

--- components/Footer.js
+++ components/Footer.js
@@ -29,7 +29,7 @@ export default function Footer() {
                                     <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
                                 </svg>
                             </a>
-                            <a href="https://www.facebook.com/profile.php?id=61585607765605" target="_blank" rel="noopener noreferrer" className="social-link" aria-label="Facebook">
+                            <a href="https://www.facebook.com/share/1AeCBUgDf5/" target="_blank" rel="noopener noreferrer" className="social-link" aria-label="Facebook">
                                 <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
                                     <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
                                 </svg>
@@ -43,7 +43,7 @@ export default function Footer() {
                             <Link href="/shop" className="footer-link">All Products</Link>
                             <Link href="/collections" className="footer-link">Collections</Link>
                             <Link href="/collections/shawl" className="footer-link">Shawls</Link>
-                            <Link href="/collections/men-stitched" className="footer-link">Men's Collection</Link>
+                            <Link href="/collections/men-unstitched" className="footer-link">Men's Collection</Link>
                         </nav>
                     </div>
 
